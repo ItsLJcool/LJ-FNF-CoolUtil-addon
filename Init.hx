b54d89086725d5ff38da12d9736362a75f6b340f
Landon Shields <lchammer0701@gmail.com>
2023-10-05 21:25:48 -0700

    stuff

--- Init.hx
+++ Init.hx
@@ -1,9 +1,13 @@
 package ljcoolutil;
+import haxe.Json;
+import haxe.Http;
 class Init {
 	static public var version:String = "1.0.0";
 	static public var custom:Bool = false;
 	static public function init() {
 		trace("initalize coolutil");
 		trace(version);
+		var data = Http.requestUrl("https://raw.githubusercontent.com/ItsLJcool/LJ-FNF-CoolUtil-addon/version.json");
+		trace(data);
 	}
 }
