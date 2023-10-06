dde520086bf967716ca93f6dabbcaffaa6345224
Landon Shields <lchammer0701@gmail.com>
2023-10-05 21:40:39 -0700

    asefg

--- Init.hx
+++ Init.hx
@@ -9,6 +9,7 @@ class Init {
 	static public function init() {
 		trace("initalize coolutil");
 		trace(version);
+		var data = null;
 		daThread = Thread.create(function() {
 			try {
 				var data = Http.requestUrl("https://raw.githubusercontent.com/ItsLJcool/LJ-FNF-CoolUtil-addon/main/version.json");
@@ -18,6 +19,7 @@ class Init {
 				return;
 			}
 		});
+		if (data == null) return;
 		data = Json.parse(data);
 		trace(data.version);
 	}
