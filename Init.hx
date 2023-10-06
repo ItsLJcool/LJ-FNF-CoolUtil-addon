3ff0cf9e253f8b563a4926e5cf1f0bd674c37346
Landon Shields <lchammer0701@gmail.com>
2023-10-05 21:43:27 -0700

    asgasg

--- Init.hx
+++ Init.hx
@@ -11,15 +11,17 @@ class Init {
 		var data = null;
 		var daThread:Thread = Thread.create(function() {
 			try {
-				data = Http.requestUrl("https://raw.githubusercontent.com/ItsLJcool/LJ-FNF-CoolUtil-addon/main/version.json");
+				var data = Http.requestUrl("https://raw.githubusercontent.com/ItsLJcool/LJ-FNF-CoolUtil-addon/main/version.json");
+				doVersionCheck(data);
 			} catch(e) {
 				trace(e.details());
 				trace(e.stack.toString());
 				return;
 			}
 		});
-		if (data == null) return;
-		data = Json.parse(data);
-		trace(data.version);
+	}
+	function doVersionCheck(data) {
+		var jsonVersion = Json.parse(data);
+		trace(jsonVersion.version);
 	}
 }
